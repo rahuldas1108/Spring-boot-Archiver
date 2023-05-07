b44b34289d0cec120e2981a0e2d70899c33e4e66
rahuldas1108 <rahuldas1108@gmail.com>
2023-05-07 16:47:09 +0530

    first commit

---
+++ src/main/java/com/example/SpringbootArchiver/SpringBootArchiverApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootArchiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootArchiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootArchiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringbootArchiver/SpringBootArchiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootArchiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootArchiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
